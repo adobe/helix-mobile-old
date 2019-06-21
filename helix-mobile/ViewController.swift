4bc9ee44b6cc988c83d38284ea954fc92ca8be1b
constance yu <faithwyu@gmail.com>
2019-06-21 16:13:59 -0700

    Initial Commit

---
+++ helix-mobile/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  helix-mobile
+//
+//  Created by conyu on 6/21/19.
+//  Copyright © 2019 helix. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
